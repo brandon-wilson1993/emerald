cc9a4a89c7bbdcf424b0c98ec305556f6fe8b4f4
brandon-wilson1993 <brandon.j.wilson.1993@gmail.com>
2025-03-05 22:25:02 -0600

    Initial commit

---
+++ src/main/java/com/emerald/emerald/EmeraldApplication.java
@@ -0,0 +1,13 @@
+package com.emerald.emerald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmeraldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmeraldApplication.class, args);
+	}
+
+}
